3ed81d41614f0d433c4d01cf9d53ac5db6f0f67e
xxyyttxx_mini <893166601@qq.com>
2017-07-21 02:05:16 +0800

    New proj: 测试板子和固件 LED 按键
    LED 每0.1s反转一次；按一次按键，这个反转时间增加一倍。
    0.1s->0.2s->0.4s->0.8s->1.6s->3.2s->...
    测试成功。

---
+++ project/p1-nucleo64-test/application/main.c
@@ -0,0 +1,100 @@
+#include "main.h"
+#include "stm32f4xx_conf.h"
+#include "button.h"
+#include "LED.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t uwTimingDelay;
+RCC_ClocksTypeDef RCC_Clocks;
+int s = 100;
+int touch = 0;
+
+/* Private function prototypes -----------------------------------------------*/
+static void Delay(__IO uint32_t nTime);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+ /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, 
+       refer to system_stm32f4xx.c file */
+
+  /* SysTick end of count event each 1ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
+  
+  /* Add your application code here */	
+	GPIO_init_4_B1();
+	GPIO_init_4_LD2();
+     
+  /* Infinite loop */
+  for (int light = 0;;)
+	{
+		Delay(s);
+		set_LD2(light=!light);
+	}
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  uwTimingDelay = nTime;
+
+  while(uwTimingDelay != 0)
+		if (touch) {
+			if (!get_B1) {
+				touch = 0;
+				s *= 2;
+			}
+		} else if (get_B1)
+			touch = 1;
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (uwTimingDelay != 0x00)
+  { 
+    uwTimingDelay--;
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
