c95543725df0f17037136adc366b402be3de1b3c
xxyyttxx_air <893166601@qq.com>
2017-07-30 16:09:30 +0800

    修改原子哥串口代码使之适应nucleo板
    1. 去掉对原子哥sys.h的依赖（本来就不需要）
    2. usart1->usart2 (st-link)
    	- 直接替换（函数／寄存器指针／……）
    	- 两个串口的总线不同，时钟使能调用不同函数
    3. PA9/PA10 -> PA2/PA3
    
    4. "usart.h"也要修改<1/2>

--- firmware/uart-nucleo/usart.c
+++ firmware/uart-nucleo/usart.c
@@ -1,4 +1,3 @@
-#include "sys.h"
 #include "usart.h"	
 ////////////////////////////////////////////////////////////////////////////////// 	 
 //如果使用ucos,则包括下面的头文件即可.
@@ -8,7 +7,7 @@
 //////////////////////////////////////////////////////////////////////////////////	 
 //本程序只供学习使用，未经作者许可，不得用于其它任何用途
 //ALIENTEK STM32F4探索者开发板
-//串口1初始化		   
+//串口2初始化		   
 //正点原子@ALIENTEK
 //技术论坛:www.openedv.com
 //修改日期:2014/6/10
@@ -26,7 +25,7 @@
 //1,修改串口初始化IO的bug
 //2,修改了USART_RX_STA,使得串口最大接收字节数为2的14次方
 //3,增加了USART_REC_LEN,用于定义串口最大允许接收的字节数(不大于2的14次方)
-//4,修改了EN_USART1_RX的使能方式
+//4,修改了EN_USART2_RX的使能方式
 //V1.5修改说明
 //1,增加了对UCOSII的支持
 ////////////////////////////////////////////////////////////////////////////////// 	  
@@ -44,21 +43,21 @@ struct __FILE
 
 FILE __stdout;       
 //定义_sys_exit()以避免使用半主机模式    
-_sys_exit(int x) 
+void _sys_exit(int x) 
 { 
 	x = x; 
 } 
 //重定义fputc函数 
 int fputc(int ch, FILE *f)
 { 	
-	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
-	USART1->DR = (u8) ch;      
+	while((USART2->SR&0X40)==0);//循环发送,直到发送完毕   
+	USART2->DR = (u8) ch;      
 	return ch;
 }
 #endif
  
-#if EN_USART1_RX   //如果使能了接收
-//串口1中断服务程序
+//#if EN_USART2_RX   //如果使能了接收
+//串口2中断服务程序
 //注意,读取USARTx->SR能避免莫名其妙的错误   	
 u8 USART_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
 //接收状态
@@ -67,7 +66,8 @@ u8 USART_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
 //bit13~0，	接收到的有效字节数目
 u16 USART_RX_STA=0;       //接收状态标记	
 
-//初始化IO 串口1 
+//初始化IO 串口2
+
 //bound:波特率
 void uart_init(u32 bound){
    //GPIO端口设置
@@ -76,38 +76,38 @@ void uart_init(u32 bound){
 	NVIC_InitTypeDef NVIC_InitStructure;
 	
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE); //使能GPIOA时钟
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);//使能USART1时钟
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);//使能USART2时钟
  
-	//串口1对应引脚复用映射
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1); //GPIOA9复用为USART1
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1); //GPIOA10复用为USART1
+	//串口2对应引脚复用映射
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_USART2); //GPIOA2复用为USART2
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_USART2); //GPIOA3复用为USART2
 	
-	//USART1端口配置
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10; //GPIOA9与GPIOA10
+	//USART2端口配置
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3; //GPIOA2与GPIOA3
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用功能
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	//速度50MHz
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; //推挽复用输出
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; //上拉
-	GPIO_Init(GPIOA,&GPIO_InitStructure); //初始化PA9，PA10
+	GPIO_Init(GPIOA,&GPIO_InitStructure); //初始化PA2，PA3
 
-   //USART1 初始化设置
+   //USART2 初始化设置
 	USART_InitStructure.USART_BaudRate = bound;//波特率设置
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
 	USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
-  USART_Init(USART1, &USART_InitStructure); //初始化串口1
+  USART_Init(USART2, &USART_InitStructure); //初始化串口2
 	
-  USART_Cmd(USART1, ENABLE);  //使能串口1 
+  USART_Cmd(USART2, ENABLE);  //使能串口2 
 	
-	//USART_ClearFlag(USART1, USART_FLAG_TC);
+	//USART_ClearFlag(USART2, USART_FLAG_TC);
 	
-#if EN_USART1_RX	
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启相关中断
+#if EN_USART2_RX	
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);//开启相关中断
 
-	//Usart1 NVIC 配置
-  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;//串口1中断通道
+	//Usart2 NVIC 配置
+  NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;//串口2中断通道
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3;//抢占优先级3
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority =3;		//子优先级3
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
@@ -118,15 +118,15 @@ void uart_init(u32 bound){
 }
 
 
-void USART1_IRQHandler(void)                	//串口1中断服务程序
+void USART2_IRQHandler(void)                	//串口2中断服务程序
 {
 	u8 Res;
 #if SYSTEM_SUPPORT_OS 		//如果SYSTEM_SUPPORT_OS为真，则需要支持OS.
 	OSIntEnter();    
 #endif
-	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
+	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
 	{
-		Res =USART_ReceiveData(USART1);//(USART1->DR);	//读取接收到的数据
+		Res =USART_ReceiveData(USART2);//(USART2->DR);	//读取接收到的数据
 		
 		if((USART_RX_STA&0x8000)==0)//接收未完成
 		{
@@ -151,7 +151,7 @@ void USART1_IRQHandler(void)                	//串口1中断服务程序
 	OSIntExit();  											 
 #endif
 } 
-#endif	
+//#endif	
 
  
 
