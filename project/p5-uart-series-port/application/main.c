e608fcc76ba6f92d935869be48e1304e7eaa7253
xxyyttxx_air <893166601@qq.com>
2017-07-30 16:14:54 +0800

    【p5】增加串口测试工程
    在死循环中，输出一个变量的值，并且改变量自增。
    测试C99 可变长参数列表宏 [可变参数宏](https://zh.wikipedia.org/wiki/可变参数宏)
    
    ps: air-pd-xp-keil5.14 的uvprojx 和 5.24 不完全一样。
    git的时候让不让它commit？头疼呀。（破事）

---
+++ project/p5-uart-series-port/application/main.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "stm32f4xx_conf.h"
+#include "usart.h"
+
+/* Private typedef -----------------------------------------------------------*/
+#define printf_(s, ...) printf("file:%s\tline:%d\t" s, __FILE__, __LINE__, __VA_ARGS__)
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @retval None
+  */
+int main(void)
+{
+ /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, 
+       refer to system_stm32f4xx.c file */
+
+  /* Add your application code here */
+	int c = 0;
+	uart_init(115200);
+	for (;;) {
+		printf_("c = %d \r\n", c++);
+	}
+
+  /* Infinite loop */
+  //for (;;);
+}
