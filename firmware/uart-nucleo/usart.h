ae70cd037567baf834ac9ce61f986b25b609f159
xxyyttxx_air <893166601@qq.com>
2017-11-09 16:05:18 +0800

    HC-05和F411相性不和?
    单独的UART2 + ST-LINK 可以。
    USB-TTL + HC-05 可以。
    然后UART2 + HC-05 就不行了。
    
    改低了波特率还是不行。
    硬件拆了SB13、SB14。还是不行。
    绝望。

--- firmware/uart-nucleo/usart.h
+++ firmware/uart-nucleo/usart.h
@@ -30,7 +30,7 @@
 //4,修改了EN_USART2_RX的使能方式
 ////////////////////////////////////////////////////////////////////////////////// 	
 #define USART_REC_LEN  			200  	//定义最大接收字节数 200
-#define EN_USART2_RX 			1		//使能（1）/禁止（0）串口1接收
+#define EN_USART2_RX 			1		//使能（1）/禁止（0）串口2接收
 	  	
 extern u8  USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符 
 extern u16 USART_RX_STA;         		//接收状态标记	
--- project/p5-uart-series-port/application/main.c
+++ project/p5-uart-series-port/application/main.c
@@ -1,5 +1,6 @@
 #include "stm32f4xx_conf.h"
 #include "usart.h"
+#include "delay.h"
 
 /* Private typedef -----------------------------------------------------------*/
 #define printf_(s, ...) printf("file:%s\tline:%d\t" s, __FILE__, __LINE__, __VA_ARGS__)
@@ -15,15 +16,17 @@
   */
 int main(void)
 {
+	
  /*!< At this stage the microcontroller clock setting is already configured, 
        this is done through SystemInit() function which is called from startup
        files before to branch to application main.
        To reconfigure the default setting of SystemInit() function, 
        refer to system_stm32f4xx.c file */
-
+	delayinit();
+	delay(2000);
   /* Add your application code here */
 	int c = 0;
-	uart_init(115200);
+	uart_init(38400);
 	for (;;) {
 		printf_("c = %d \r\n", c++);
 	}
