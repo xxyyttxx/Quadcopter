2a6a93f8ab59adbf4ae33ce791e5d522f9ebd0ca
xxyyttxx_air <893166601@qq.com>
2017-11-11 16:22:49 +0800

    [p6]new project

---
+++ project/p6-PWM-motor/application/motor-PWM.h
@@ -0,0 +1,29 @@
+/**
+  @file    motor-PWM.h
+  @author  tjua @ ES-SS-UESTC © 2017
+  @version V1.0
+  @date    20171111
+  @brief   a header file declares the interface of motor control.
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __motor_PWM
+#define __motor_PWM
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __motor_PWM */
