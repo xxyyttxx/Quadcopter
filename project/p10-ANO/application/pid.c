0a698b51a01908071c8d19af54ba2fa86a7cb0b4
xxyyttxx_air <893166601@qq.com>
2017-12-31 12:31:01 +0800

    修改控制算法...
    // 可编译未测试的代码
    - 增加积分项
    - 修改映射
    
    // 高危！！
    如果遥控器没有关机/归零，然后给四轴上电烧录程序，四轴会打到人。。。

--- project/p10-ANO/application/pid.c
+++ project/p10-ANO/application/pid.c
@@ -1,3 +1,4 @@
+#include "delay.h"
 #include "pid.h"
 #include "Attitude.h"
 #include "PWM-RCV.h"
@@ -6,7 +7,7 @@
 
 #define max_angle_pr 20
 #define max_angle_yaw 180
-#define max_reaction 60 /// range_pwm 映射限定参数 //// 等待讨论
+#define I_limit_init 200
 
 PID_Typedef pitch_angle_PID;    // pitch角度PID
 PID_Typedef roll_angle_PID;
@@ -34,14 +35,17 @@ void PID_init(void)
     pitch_angle_PID.P = 1;
     pitch_angle_PID.I = 0;
     pitch_angle_PID.D = 0;
+    pitch_angle_PID.iLimit = I_limit_init;
 
     roll_angle_PID.P = 1;
     roll_angle_PID.I = 0;
     roll_angle_PID.D = 0;
+    roll_angle_PID.iLimit = I_limit_init;
 
     yaw_angle_PID.P = 1;
     yaw_angle_PID.I = 0;
     yaw_angle_PID.D = 0;
+    yaw_angle_PID.iLimit = I_limit_init;
 }
 
 /**
@@ -49,15 +53,19 @@ void PID_init(void)
     * @param  PID_Typedef * PID： PID结构体
               float target：      期望值
               float measure：     观测值
-              int32_t dertT：     时间间隔，用于计算积分
+              uint32_t dertT：     时间间隔，用于计算积分
     * @retval None
     */
-static void PID_postion_cal(PID_Typedef * PID, float measure, int32_t dertT)
+static void PID_postion_cal(PID_Typedef * PID, float measure, uint32_t dertT)
 {
     // 计算误差 = 期望值 - 观测值
     PID->Error = PID->Desired - measure;
     // 计算积分
-    PID->Integ += (double)PID->Error * dertT / 1000000.0;
+    PID->Integ += (double)PID->Error * dertT / 1000.0;
+#if 0
+    if (PID->Integ > PID->iLimit) PID->Integ = PID->iLimit;
+    if (PID->Integ < -PID->iLimit) PID->Integ = -PID->iLimit;
+#endif
     // 计算微分
     PID->Deriv = PID->Error - PID->PreError;
     // 计算输出
@@ -76,17 +84,15 @@ static float range_trans(uint16_t Rcvr_ch, uint16_t max_angle)
 
 static inline uint16_t range_pwm(float motor_pwm, uint16_t thr)
 {
+    thr += motor_pwm;
     if (thr<=1000) thr = 1001;
     if (thr>=2000) thr = 1999;
-    if (motor_pwm>max_reaction) motor_pwm = max_reaction;
-    if (motor_pwm<-max_reaction) motor_pwm = -max_reaction;
-    // 1000..thr
-    return 1000 + (motor_pwm/max_reaction+1)/2 * (thr-1000);
+    return thr;
 }
 
 void PID_calculate(void)
 {
-    int32_t dt = 0;
+    static uint32_t told = 0;
     
     // 从遥控接收机获取期望值
 	
@@ -94,10 +100,11 @@ void PID_calculate(void)
     pitch_angle_PID.Desired = range_trans(u16Rcvr_ch2, max_angle_pr);       // f(u16Rcvr_ch2) +
     yaw_angle_PID.Desired   = 0; // range_trans(3000-u16Rcvr_ch4, max_angle_yaw); // f(u16Rcvr_ch4) -
                                      // !!!! // 假通道，没有需求
-
-    PID_postion_cal(&roll_angle_PID,  roll,  dt); //要求平稳飞行 实际角度很小，0代表没有积分
-    PID_postion_cal(&pitch_angle_PID, pitch, dt);
-    PID_postion_cal(&yaw_angle_PID,   yaw,   dt);
+    uint32_t tnow = msTimerCounter;
+    PID_postion_cal(&roll_angle_PID,  roll,  tnow-told); //要求平稳飞行 实际角度很小，0代表没有积分
+    PID_postion_cal(&pitch_angle_PID, pitch, tnow-told);
+    PID_postion_cal(&yaw_angle_PID,   yaw,   tnow-told);
+    told = tnow;
 
     // 获取输出值
     uint16_t Thr = u16Rcvr_ch3; /// 不用转换量程，因为通道捕获和电调输出都是1000～2000
@@ -106,10 +113,17 @@ void PID_calculate(void)
     float Yaw    = yaw_angle_PID.Output;
 
     // 输出值融合到四个电机
-    motor_pwm_1 = range_pwm( - Pitch - Roll + Yaw, Thr);
-    motor_pwm_2 = range_pwm( + Pitch - Roll - Yaw, Thr);
-    motor_pwm_3 = range_pwm( + Pitch + Roll + Yaw, Thr);
-    motor_pwm_4 = range_pwm( - Pitch + Roll - Yaw, Thr);
+    if (Thr > 1100) { // 是否锁定状态
+        motor_pwm_1 = range_pwm( - Pitch - Roll + Yaw, Thr);
+        motor_pwm_2 = range_pwm( + Pitch - Roll - Yaw, Thr);
+        motor_pwm_3 = range_pwm( + Pitch + Roll + Yaw, Thr);
+        motor_pwm_4 = range_pwm( - Pitch + Roll - Yaw, Thr);
+    } else {
+        motor_pwm_1 = motor_pwm_min;
+        motor_pwm_2 = motor_pwm_min;
+        motor_pwm_3 = motor_pwm_min;
+        motor_pwm_4 = motor_pwm_min;
+    }
 }
 
 #if 0
